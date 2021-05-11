7e7bafebab9e010a86dc72c7633e8412f820197f
yuiseki <yuiseki@gmail.com>
2021-05-12 07:48:49 +0900

    first impl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,54 @@
 /// <reference types="@emotion/react/types/css-prop" />
-import tw, { css } from "twin.macro";
-import { Button } from "../components/button";
+import React from 'react';
+import Head from 'next/head';
+import tw, { css } from 'twin.macro';
+import useSWR from 'swr';
 
 const container = css`
   ${tw`mx-auto m-4 p-4 rounded bg-gray-400`}
 `;
 
-export const Home = (): JSX.Element => (
-  <div css={container}>
-    <main>
-      <h1 tw="text-5xl font-bold">
-        Nextjs App with TypeScript, ESlint, Jest, Emotion, Tailwind and Twin
-      </h1>
-      <Button />
-    </main>
-  </div>
-)
+export const Home = () => {
+  const { data } = useSWR(
+    'https://gyazo.com/api/collections/fcb91cdfffa27fb6c2ea955742fed76e/images'
+  );
+  const sortedData = data
+    ?.sort((a, b) => {
+      return (
+        new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+      );
+    })
+    .reverse();
+  return (
+    <>
+      <Head>
+        <title>いろんな投資信託に10万円づつ投資してみた結果</title>
+      </Head>
+      <div css={container}>
+        <main>
+          <h1 tw='text-5xl font-bold'>
+            いろんな投資信託に10万円づつ投資してみた結果
+          </h1>
+          <div>
+            {sortedData &&
+              sortedData.map((image) => {
+                return (
+                  <div tw='m-3' key={image.image_id}>
+                    <h2 tw='text-4xl font-bold'>
+                      {new Date(image.created_at).toLocaleDateString()}
+                    </h2>
+                    <h3>{image.created_at}</h3>
+                    <div>
+                      <img tw='h-full w-full' src={image.url} />
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
